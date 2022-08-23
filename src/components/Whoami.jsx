307597104c468d557a623312ad9e84500c4606bb
nicoescudero <nicolas.escudero00@gmail.com>
2022-08-23 20:58:23 -0300

    change message

--- src/components/Whoami.jsx
+++ src/components/Whoami.jsx
@@ -7,7 +7,7 @@ function Whoami(){
       <Navbar/>
       <main className='content'>
         <p className='text-center'>
-          Whoami!
+        Hola me llamo Nicolás tengo 24 años soy un desarrollador web autodidacta. Actualmente busco insertarme en la industria IT para poder crecer y aprender junto a personas con mayor experiencia, estoy predispuesto a los desafíos y al aprendizaje permanente.
         </p>
       </main>
     </div>
